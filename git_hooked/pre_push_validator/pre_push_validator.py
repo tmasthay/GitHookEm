45bc5d7a86406a6d1931371a215d1e6234a9ca1c
tmasthay <tyler@ices.utexas.edu>
2023-10-04 22:10:02 -0500

    asdfad

--- git_hooked/pre_push_validator/pre_push_validator.py
+++ git_hooked/pre_push_validator/pre_push_validator.py
@@ -6,7 +6,7 @@ from git_hooked.git_validator import GitValidator
 class PrePushValidator(GitValidator):
     def base(self):
         # 1. Check with black
-        black_result = os.system("black --check .")
+        black_result = os.system("black -l 80 --check .")
         if black_result != 0:
             print("Error: Code does not adhere to black's conventions!")
             exit(1)
@@ -15,10 +15,9 @@ class PrePushValidator(GitValidator):
         changed_files = (
             os.popen("git diff --staged --name-only").read().splitlines()
         )
-        input(os.getcwd())
-        print('yo')
         python_files = [f for f in changed_files if f.endswith(".py")]
 
+        input(os.getcwd())
         for py_file in python_files:
             with open(py_file, "r") as f:
                 lines = f.readlines()
