da10624853255eb58b7cbeb10960233f14258be4
tmasthay <tyler@ices.utexas.edu>
2023-10-23 12:59:38 -0500

    BUGFIX: Import cprint to pre commit validator

--- git_hook_em/pre_commit_validator/pre_commit_validator.py
+++ git_hook_em/pre_commit_validator/pre_commit_validator.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from git_validator import GitValidator
+from git_validator import GitValidator, cprint
 
 
 class PreCommitValidator(GitValidator):
