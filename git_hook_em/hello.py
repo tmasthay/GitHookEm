a2bc0ac01ff6ed2b927f806c8050799ced42ab8e
tmasthay <tyler@ices.utexas.edu>
2023-11-08 12:55:29 -0600

    CONFIG: Add the commit hooks yaml

---
+++ git_hook_em/hello.py
@@ -0,0 +1,11 @@
+# hello.py
+import sys
+
+def main(commit_msg_file_path):
+    with open(commit_msg_file_path, 'r') as f:
+        commit_msg = f.read().strip()
+    print(f"Hello World - Commit message is: {commit_msg}")
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
