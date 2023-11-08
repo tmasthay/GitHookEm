4de96b8e27d3b4875b3327b12b8fe604cde8b458
tmasthay <tyler@ices.utexas.edu>
2023-11-08 15:07:11 -0600

    FEATURE: Add ban-super-secret pre-commit hook

---
+++ git_hook_em/pre_commit_validator/ban_super_secret.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from git_validator import GitValidator, cprint
+
+
+class BanSuperSecret(GitValidator):
+    def base(self):
+        tracked_files = os.popen("git ls-files").read().strip()
+        ban = "super_secret"
+        for file in tracked_files:
+            file = file.lower()
+            ban2 = ban.replace("_", "")
+            if ban in file or ban2 in file:
+                cprint(
+                    f'Now tracking "{file}" which has "{ban}" or "{ban2}" as a'
+                    ' substring! Commit FAILED!'
+                )
+                exit(-1)
+
+
+if __name__ == "__main__":
+    BanSuperSecret(0).validate()
